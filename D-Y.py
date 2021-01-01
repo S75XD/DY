e1bd4e5f2f53506c859a42f4ce3e875e8087ac05
S75XD <66068313+S75XD@users.noreply.github.com>
2021-01-01 14:10:36 +0300

    Add files via upload

---
+++ D-Y.py
@@ -0,0 +1,24 @@
+#المكتبات المستخدمه
+from pafy import new 
+import pyfiglet
+#=======1===============
+
+#حقوق مستر اكس دي
+log = pyfiglet.figlet_format("by MrXD")
+print(log)
+#=========2============
+
+#لازم تحمل الموجودين
+#print("1-pip install pafy")
+#print("2-install youtube.dl")
+#=======3=============
+
+#الكود المستخدم
+url = input('enter you link here: ')
+video = new(url)
+dl = video.getbest()
+dl.download()
+
+#تم
+logg = pyfiglet.figlet_format("Done")
+print(logg)
