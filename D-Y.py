59f5b75e98f8382064adc6b9cd9633c30f576d2b
S75XD <66068313+S75XD@users.noreply.github.com>
2021-01-27 08:17:21 +0300

    Add files via upload

--- D-Y.py
+++ D-Y.py
@@ -4,20 +4,69 @@ import pyfiglet
 #=======1===============
 
 #حقوق مستر اكس دي
-log = pyfiglet.figlet_format("by MrXD")
-print(log)
+print('''
+
+ /$$      /$$           /$$   /$$ /$$$$$$$ 
+| $$$    /$$$          | $$  / $$| $$__  $$
+| $$$$  /$$$$  /$$$$$$ |  $$/ $$/| $$  \ $$
+| $$ $$/$$ $$ /$$__  $$ \  $$$$/ | $$  | $$
+| $$  $$$| $$| $$  \__/  >$$  $$ | $$  | $$
+| $$\  $ | $$| $$       /$$/\  $$| $$  | $$
+| $$ \/  | $$| $$      | $$  \ $$| $$$$$$$/
+|__/     |__/|__/      |__/  |__/|_______/ 
+                                           
+                                                                                      
+''')
+print('''
+=========================
+=      My rights        =
+=     snap:ss75sa       =
+=    twitter:@S75XD     =
+=   telegram:@S75XD     =
+========================= 
+''')
 #=========2============
 
 #لازم تحمل الموجودين
 #print("1-pip install pafy")
 #print("2-install youtube.dl")
 #=======3=============
-
+print('''
+[1]تشيكرز يتيوب
+[2]تحميل مقطع MP3
+[3]تحميل المقطع اعلى جوده
+[4]تحميل المقطع مع اختيار الجوده
+''')
 #الكود المستخدم
-url = input('enter you link here: ')
-video = new(url)
-dl = video.getbest()
-dl.download()
+A5 = input('ضع رقم الاداة  to 4 -->: ')
+
+if A5 == '1':
+    url = input('enter you link here: ')
+    video = new(url)
+    print(video)
+
+
+elif A5 == '2':
+    url = input('enter you link here: ')
+    video = new(url)
+    audio = video.audiostreams
+    audio[0].download()
+
+
+elif A5 =='3':
+    url = input('enter you link here: ')
+    video = new(url)
+    dl = video.getbest()
+    dl.download()
+
+
+elif A5 =='4':
+    url = input('enter you link here: ')
+    video = new(url)
+    stream = video.streams
+    for i in stream:
+        print(i)
+
 
 #تم
 logg = pyfiglet.figlet_format("Done")
